a8f074e88ca5f35682265b383d76aaa40cdbad1d
Lucas Tatin <lucas.eduardoz2001@gmail.com>
2024-05-20 21:45:23 -0300

    Conversor de Moedas - Banco de Dados Implementado

--- lib/pages/pagLogin.dart
+++ lib/pages/pagLogin.dart
@@ -14,41 +14,43 @@ class PagLogin extends StatelessWidget {
         title: Text('Login Conversor de Moedas'), // Título centralizado
         centerTitle: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CampoLogin(
-              icon: Icons.person,
-              hintText: 'Digite seu login',
-              obscureText: false,
-              controller: _loginController,
-            ),
-            SizedBox(height: 20.0),
-            CampoLogin(
-              icon: Icons.lock,
-              hintText: 'Digite sua senha',
-              obscureText: true,
-              controller: _senhaController,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                if (_loginController.text == '123' && _senhaController.text == '123') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ConverteMoedas()), // Remova o parâmetro historico
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Usuário ou senha incorretos, favor digitar corretamente')),
-                  );
-                }
-              },
-              child: Text('Entrar'),
-            ),
-          ],
+      body: SingleChildScrollView( // Adicionando SingleChildScrollView aqui
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CampoLogin(
+                icon: Icons.person,
+                hintText: 'Digite seu login',
+                obscureText: false,
+                controller: _loginController,
+              ),
+              SizedBox(height: 20.0),
+              CampoLogin(
+                icon: Icons.lock,
+                hintText: 'Digite sua senha',
+                obscureText: true,
+                controller: _senhaController,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_loginController.text == '123' && _senhaController.text == '123') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ConverteMoedas()),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Usuário ou senha incorretos, favor digitar corretamente')),
+                    );
+                  }
+                },
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
